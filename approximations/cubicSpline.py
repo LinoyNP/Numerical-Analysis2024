0f663faf8a5c310c8366e13e3ff474cbdb003d74
LinoyNP <129064940+LinoyNP@users.noreply.github.com>
2024-04-07 23:26:06 +0300

    cubiSpline - full + natural

--- approximations/cubicSpline.py
+++ approximations/cubicSpline.py
@@ -1,24 +1,62 @@
 import numpy as np
+import math as m
 
-def cubicSpline(table_points, point, type = False):
+def cubicSpline(table_points, point, y1_derivative, y2_derivative):
     table_size = len(table_points)
-    matrix = [[0 for x in range(table_size)] for y in range(table_size)]
-    if type: # this is natural cubicSpline
-        #matrix[0][0] = matrix[]
-        for i in range(1, table_size):
-            h0 = table_points[i][0] - table_points[i-1][0]
-            h1 = table_points[i+1][1] - table_points[i][1]
-            for j in range(i-1, i+2):
-                if i != table_size-1:  # this is not last line
-                    matrix[i][j].append(1/6)
+    #matrix = [[0 for x in range(table_size)] for y in range(table_size)]  # Initialize the matrix to zeros
+    matrix = list()
+    matrix.append(list())
+    h = list()
+    for i in range(table_size - 1):
+        h.append(table_points[i + 1][0] - table_points[i][0])  # h = xi - xi-1
+    #matrix[0][0] = matrix[]
+    # first line
+    for j in range(table_size):
+        if j == 0:
+            matrix[0].append(h[0]/3)
+        elif j == 1:
+            matrix[0].append(h[0]/6)
+        else:
+            matrix[0].append(0)
 
-    else:
-        h = table_points[1][0] - table_points[0][0]
-        matrix[0][0] = 1/3 * h
-        matrix[0][1] = 1/6 * h
-        for i in range(1, table_size):
+    for i in range(1, table_size-1):
+        matrix.append(list())
+        for j in range(table_size):
+            if j == i-1: matrix[i].append(h[i-1]/6)
+            elif j == i: matrix[i].append((h[i-1] + h[i])/3)
+            elif j == i+1: matrix[i].append(h[i]/6)
+            else:
+                matrix[i].append(0)
 
-            h = table_points[i+1][0] - table_points[i][0]
+    #last line:
+    matrix.append(list())
+    for row in range(table_size):
+        if row == table_size-2:
+            matrix[table_size-1].append(h[-1] / 6)
+        elif row == table_size-1:
+            matrix[table_size-1].append(h[-1] / 3)
+        else:
+            matrix[table_size-1].append(0)
+
+    d = list()
+    for i in range(table_size):
+        if i == 0:  # d0=(y1-y0)/h0 - y'(0)
+            d.append(((table_points[1][1]-table_points[0][1])/h[0]) - y1_derivative)
+        elif i == table_size-1:  # dn= y'(n)- (yn-yn-1)/h0
+            d.append(y2_derivative - ((table_points[table_size-1][1] - table_points[table_size-2][1])/h[0]))
+        else:
+            d.append(((table_points[i + 1][1] - table_points[i][1]) / h[i]) - ((table_points[i][1] - table_points[i - 1][1]) / h[i - 1]))
+
+    print("matrix: " + str(matrix))
+    print("vector b: " + str(d))
+
+    # get m vector
+    result = np.zeros_like(d, dtype=np.double)
+    print("\nJacobi middle results: ")
+    M = (jacobi_iterative(matrix, d, result))
+    print("\nvector M: " + str(list(map(float, M))))
+
+
 
 from Iterative_methods_matrix.Jacobi import jacobi_iterative
 from sympy import *
@@ -111,10 +149,16 @@ def natural_cubic_spline(f, x0):
     print("s" + str(loc - 1) + "(" + str(x0) + ") = " + str(float(s.subs(x, x0))))
 
 
+
 if __name__ == '__main__':
-    f = [(1, 1), (2, 2), (3, 1), (4, 1.5), (5, 1)]
-    x0 = 4.5
+    """f = [(1, 1), (2, 2), (3, 1), (4, 1.5), (5, 1)]
+    x0 = 4.5"""
+    f = [(0,0), (m.pi/6, 0.5), (m.pi/4, 0.7072), (m.pi/2, 1)]
+    x0 = m.pi/3
+    y_0_deri = 1
+    y_n_deri = 0
 
     print("func: " + str(f))
     print("x0 = " + str(x0) + "\n")
-    natural_cubic_spline(f, x0)
+    #natural_cubic_spline(f, x0)
+    cubicSpline(f, x0, y_0_deri, y_n_deri)
